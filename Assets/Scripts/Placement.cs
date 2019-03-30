5530dae6e35dd9357fc69403a941b3813d77dcce
sherlynng <sherlyn.xinler@gmail.com>
2019-03-30 21:41:56 +0800

    merge placement and ui

---
+++ Assets/Scripts/Placement.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placement : MonoBehaviour
+{
+    public GameObject placementIndicator;
+    public GameObject objectToPlace;
+    public Camera arCamera;
+    public GameObject objects;
+
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+    private List<GameObject> furnitures = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SSTools.ShowMessage("Start", SSTools.Position.top, SSTools.Time.twoSecond);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    public void SetObjectToPlace(GameObject obj)
+    {
+        objectToPlace = obj;
+    }
+
+    public void PlaceObject()
+    {
+        var newObject = Instantiate(objectToPlace, placementPose.position, placementPose.rotation * objectToPlace.transform.rotation);
+        newObject.transform.parent = objects.transform;
+        furnitures.Add(newObject);
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            string position = placementPose.position.ToString();
+            string rotation = " R: " + placementPose.rotation.ToString();
+            SSTools.ShowMessage(position + rotation, SSTools.Position.top, SSTools.Time.oneSecond);
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+       // Vector3 screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        Vector3 screenCenter = arCamera.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        RaycastHit rayHit;
+
+        //if (Physics.Raycast(Camera.current.ScreenPointToRay(screenCenter), out rayHit))
+        if (Physics.Raycast(arCamera.ScreenPointToRay(screenCenter), out rayHit))
+        {
+           // SSTools.ShowMessage("Raycast Hit", SSTools.Position.top, SSTools.Time.oneSecond);
+
+            if (rayHit.collider.gameObject.CompareTag("PlacementPlane"))
+            {
+                //SSTools.ShowMessage("Compare Tag", SSTools.Position.top, SSTools.Time.oneSecond);
+                placementPoseIsValid = true;
+
+                placementPose.position = rayHit.point;
+
+                Vector3 cameraForward = arCamera.transform.forward;
+                Vector3 cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+                placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+            }
+        }
+    }
+
+    public void RemoveObject()
+    {
+        Touch touch = Input.GetTouch(0);
+        RaycastHit rayHit;
+
+        if (Physics.Raycast(arCamera.ScreenPointToRay(touch.position), out rayHit))
+        {
+            if (!rayHit.collider.gameObject.CompareTag("PlacementPlane"))
+            {
+                Destroy(rayHit.transform.gameObject);
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        foreach (GameObject furniture in furnitures)
+        {
+            Destroy(furniture);
+        }
+
+        furnitures = new List<GameObject>();
+    }
+}
---
+++ Assets/Scripts/SubMenu.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class SubMenu : MonoBehaviour
+{
+    public MenuManager menuManager;
+    public Placement interaction;
+    public GameObject buttonPrefab;
+    private string menuName;
+
+    private Button[] assetButtons = new Button[0];
+
+    public void init(string currMenu)
+    {
+        menuName = currMenu;
+
+        // disable all furniture button on startup
+        //disableAllButtons();
+
+        // delete existing buttons
+        deleteAllButtons();
+
+        // clear all selected buttons
+        Reset();
+
+        GameObject[] assetPrefab = Resources.LoadAll<GameObject>(currMenu);
+        assetButtons = new Button[assetPrefab.Length];
+
+        // replace text with name of furniture prefab in Resources folder
+        for (int i = 0; i < assetPrefab.Length; i++)
+        {
+
+            // instantiate a new button based on prefabs loaded from Resources folder
+            GameObject createdButton = Instantiate(buttonPrefab, transform);
+            assetButtons[i] = createdButton.GetComponent<Button>();
+
+            assetButtons[i].gameObject.name = assetPrefab[i].name;
+            assetButtons[i].GetComponentInChildren<Text>().text = assetPrefab[i].name;
+
+            assetButtons[i].onClick.AddListener(() => SetActivePrefab(createdButton.GetComponent<Button>()));
+        }
+    }
+
+    void deleteAllButtons()
+    {
+        if (assetButtons.Length > 0)
+        {
+            foreach (Button button in assetButtons)
+            {
+                Destroy(button.gameObject);
+            }
+        }
+    }
+
+    void disableAllButtons()
+    {
+        foreach (Button button in assetButtons)
+        {
+            if (button.tag == "FurnitureButton")
+            {
+                button.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void SetActivePrefab(Button furnitureButton)
+    {
+        Reset();
+        //furnitureButton.GetComponent<Image>().color = Color.grey;
+        interaction.SetObjectToPlace(Resources.Load<GameObject>(menuName + "/" + furnitureButton.name));
+        interaction.PlaceObject();
+    }
+
+    private void Reset()
+    {
+        if (assetButtons.Length > 0)
+        {
+            foreach (Button button in assetButtons)
+            {
+                //button.GetComponent<Image>().color = Color.white;
+            }
+        }
+    }
+}
