07975e54ee1c78b82812736b9cff7a384ef305bb
marlenekoh <marlenekohxml@gmail.com>
2019-04-16 12:41:00 +0800

    add blocker for instructions panel

---
+++ Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            RaycastHit rayHit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(touch.position), out rayHit))
+            {
+                GameObject hitObject = rayHit.collider.gameObject;
+                if (hitObject.CompareTag("3DButton"))
+                {
+
+                }
+            }
+        }
+    }
+}
